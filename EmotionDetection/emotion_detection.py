1d812d3f84dadf070f8ece90aa7ae5223b2b2272
EmilianoAnaya <gael.anaya@alumnos.udg.mx>
2024-12-19 19:57:11 -0500

    Final Project Finished

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,6 +6,16 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, headers=headers, json=myobj)
+
+    if response.status_code == 400:
+        return {
+        'anger' : None,
+        'disgust' : None,
+        'fear' : None,
+        'joy' : None,
+        'sadness' : None,
+        'dominant_emotion' : None
+    }
     
     formatted_response = json.loads(response.text)
 
--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+'''
+Server for the Emotion Detection app
+'''
+
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,13 +9,23 @@ app = Flask("Emotion Detection")
 
 @app.route('/')
 def index():
+    '''
+    Renders the index.html page.
+    '''
     return render_template('index.html')
 
 @app.route("/emotionDetector")
 def emo_detector():
+    '''
+    Analyzes the given text from the index.html page and returns the detected emotions along
+    with the dominant one
+    '''
     text_to_analyze = request.args.get("textToAnalyze")
 
     results = emotion_detector(text_to_analyze)
+
+    if results['dominant_emotion'] is None:
+        return "Invalid text! Please try again!."
 
     anger_score = results['anger']
     disgust_score = results['disgust']
@@ -20,7 +34,12 @@ def emo_detector():
     sadness_score = results['sadness']
     dominant_emotion = results['dominant_emotion']
 
-    return f"For the given statement, the system response is 'anger': {anger_score}, 'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score} and 'sadness': {sadness_score}. The dominant emotion is {dominant_emotion}."
+    return(
+        f"For the given statement, the system response is 'anger': {anger_score},"
+        f"'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score} and"
+        f"'sadness': {sadness_score}."
+        f"The dominant emotion is {dominant_emotion}."
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
